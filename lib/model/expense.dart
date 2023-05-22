765b6dc3b48f6bce8d3f034469cfbb1e477b5311
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-05-22 16:38:03 +0530

    expense buckets

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -36,3 +36,23 @@ class Expense {
   }
 }
 
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpense {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
